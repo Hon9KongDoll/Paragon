61978b4b282c3abc52c2ce4a53b63bf5d4af5684
Hon9KongDoll <1483033869@qq.com>
2026-04-16 00:22:57 +0800

    Add the Basic Components of the player

--- Source/Paragon/Private/Character/PlayerCharacter.cpp
+++ Source/Paragon/Private/Character/PlayerCharacter.cpp
@@ -1,6 +1,16 @@
 #include "Character/PlayerCharacter.h"
 
+//Engine
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
 APlayerCharacter::APlayerCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArm->SetupAttachment(GetRootComponent());
+
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 }
