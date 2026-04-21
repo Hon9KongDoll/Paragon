2f571e3ce9530302a3fc118cb95c6e1c2e750423
Hon9KongDoll <1483033869@qq.com>
2026-04-21 22:12:15 +0800

    Add the Health and Mana Attribute to the Attribute Set

--- Source/Paragon/Public/AbilitySystem/ParagonAttributeSet.h
+++ Source/Paragon/Public/AbilitySystem/ParagonAttributeSet.h
@@ -2,11 +2,34 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "ParagonAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 UCLASS()
 class PARAGON_API UParagonAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY()
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UParagonAttributeSet, Health)
+
+	UPROPERTY()
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UParagonAttributeSet, MaxHealth)
+
+	UPROPERTY()
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UParagonAttributeSet, Mana)
+
+	UPROPERTY()
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UParagonAttributeSet, MaxMana)
 };
